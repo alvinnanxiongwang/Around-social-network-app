c09c5ee0b3a08c88fb1f5eff9c49df037342720f
alvinnanxiongwang <alvinwang1026@gmail.com>
2022-05-14 18:03:38 +0000

    added token-based authentication

---
+++ around/constants/constants.go
@@ -0,0 +1,10 @@
+package constants
+
+const (
+	USER_INDEX  = "user"
+	POST_INDEX  = "post"
+	ES_URL      = "http://10.128.0.2:9200"
+	ES_USERNAME = "alvin"
+	ES_PASSWORD = "alvin123"
+	GCS_BUCKET  = "alvin-around-01"
+)
